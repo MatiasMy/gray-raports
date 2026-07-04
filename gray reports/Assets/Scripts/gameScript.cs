3a6f267c08ea560d7e6ad434857867f48f8da093
myllmat22 <matiasmyllymaki05@gmail.com>
2026-07-04 03:37:40 +0300

    notes app

--- gray reports/Assets/Scripts/gameScript.cs
+++ gray reports/Assets/Scripts/gameScript.cs
@@ -6,10 +6,46 @@ using UnityEngine.SceneManagement;
 public class gameScript : MonoBehaviour
 {
     public GameObject emailApp;
+    public GameObject notesApp;
+    public GameObject solitaireErrorApp;
+    public GameObject newsApp;
+    public GameObject someApp;
+    public GameObject canApp;
+
     public GameObject desktop;
     public void openEmailApp()
     {
         emailApp.SetActive(true);
+        desktop.SetActive(false);
+    }
+
+    public void openNotesApp()
+    {
+        notesApp.SetActive(true);
+        desktop.SetActive(false);
+    }
+
+    public void openSolitaireErrorApp()
+    {
+        solitaireErrorApp.SetActive(true);
+        desktop.SetActive(false);
+    }
+
+    public void openNewsApp()
+    {
+        newsApp.SetActive(true);
+        desktop.SetActive(false);
+    }
+
+    public void openSomeApp()
+    {
+        someApp.SetActive(true);
+        desktop.SetActive(false);
+    }
+
+    public void openCanApp()
+    {
+        canApp.SetActive(true);
         desktop.SetActive(false);
     }
 }
